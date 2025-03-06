214add9d77686ec807ae77b8d768de3b32c5ccc3
AndyC <andrew@asc-solutions.co.uk>
2025-03-06 12:46:38 +0000

    Create get-token.py

---
+++ get-token.py
@@ -0,0 +1,33 @@
+import requests
+
+client_id = ''
+client_secret = ''
+scope = 'api://6af8xxxxxxxxxxxxxxxxxxxxxxxx/.default'
+token_url = "https://login.microsoftonline.com/2b1aaaaa-yyy-xxxx-a96c-sdgsgvzdgzdsg/oauth2/v2.0/token"
+
+http_headers = {
+    'Content-Type':  'application/x-www-form-urlencoded',
+}
+http_body = {
+    "client_id": {client_id},
+    "scope": {scope},
+    "client_secret": {client_secret},
+    "grant_type": "client_credentials",
+}
+response = requests.post(token_url, data=http_body, headers=http_headers)
+print(response.json()["access_token"])
+
+# check if succesful
+#if response.returncode == 0:
+#    try:
+#        token_response = json.loads(response.stdout)
+#        access_token = token_response.get('access_token')#
+
+        #if access_token:
+         #   print(f"Access token: {access_token}")
+        #else:
+       #     print("Access token not found in response.")
+    #except json.JSONDecodeError:
+     #   print("Failed to parse JSON response.")
+#else:
+ #   print(f"Error executing curl command: {response.stderr}")
